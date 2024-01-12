7a00af62007172a62dda70115ddb17686d48764d
khalifa boulbayem <khalifablbm@gmail.com>
2024-01-12 16:25:10 +0000

    crear routacion

--- public/index.php
+++ public/index.php
@@ -1,2 +1,36 @@
-<?php 
-echo "initial";
+<?php
+
+use Philo\Blade\Blade;
+
+require_once "../vendor/autoload.php";
+//blade
+// $views = "../src/views";
+// $cache = "../cache";
+
+// $blade = new Blade($views, $cache);
+
+$namespace = "Juego\\Ifriend\\";
+// Router system
+$router = new AltoRouter();
+// List of routes
+require_once '../src/routers/router.php';
+
+// End of list
+$match = $router->match();
+
+if($match) {
+ $target = $match["target"];
+ if(is_string($target) && strpos($target, "#") !== false) {
+     list($controller, $action) = explode("#", $target);
+     $controller = $namespace."Controllers\\" . $controller;
+     $controller = new $controller($router);
+     $controller->$action($match["params"]);
+ } else {
+     if(is_callable($match["target"])) 
+call_user_func_array($match["target"], $match["params"]);
+     else require $match["target"];
+ }
+} else {
+ echo "Ruta no válida";
+ die();
+}
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Juego\Ifriend\Controllers;
+
+class DefaultController {
+  
+  public function index() {
+    echo "Hello Index from default";
+    //echo $blade->view()->make('dafault')->render();
+
+  }
+
+  
+}
