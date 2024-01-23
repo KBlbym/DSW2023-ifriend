c7148d41f6e2f7f1a27840c1b27e3786bf25e275
Atteneri Estévez <atteeste2003@gmail.com>
2024-01-23 19:23:30 +0000

    edit users

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -24,7 +24,32 @@ class UserController extends Controller{
     echo $this->blade->make("user.create", compact("router"))->render();
   }
 
-  public function post($params){
-    var_dump($params);
+  public function edit($params){
+    $id = $params["id"];
+    $user = User::find($id);
+    $router = $this->router;
+    echo $this->blade->make("user.edit", compact("user","router"))->render();
+  }
+
+  public function update($params){
+    //Validación de los datos
+    $id = $params["id"];
+    $user = User::find($id);
+    $user->name = $_POST["name"];
+    $user->password = $_POST["password"];
+    $user->mail = $_POST["mail"];
+    $user->save();
+    $router = $this->router;
+    header("Location: /user");
+  }
+
+  public function post(){
+    //Validación de los datos
+    $user = new User();
+    $user->name = $_POST["name"];
+    $user->password = $_POST["password"];
+    $user->mail = $_POST["mail"];
+    $user->save();
+    header("Location: /user");
   }
 }
--- src/views/user/list.blade.php
+++ src/views/user/list.blade.php
@@ -3,6 +3,7 @@
 @section("title", "Bienvenido a Lista de Usuarios")
 
 @section("content")
+<a href="{{$router->generate('user_create')}}" class="btn btn-success">Crear Usuario</a>
 <h2>Tabla de usuarios</h2>
 <!-- <pre>{{$users}}</pre> -->
 <table class="table table-success table-striped">
@@ -23,7 +24,10 @@
       <td>{{$user->name}}</td>
       <td>{{$user->password}}</td>
       <td>{{$user->mail}}</td>
-      <td><a href="{{$router->generate('user_delete',['id' => $user->id])}}" class="btn btn-danger">Eliminar</a></td>
+      <td>
+      <a href="{{$router->generate('user_edit',['id' => $user->id])}}" class="btn btn-warning">Editar</a>
+      <a href="{{$router->generate('user_delete',['id' => $user->id])}}" class="btn btn-danger">Eliminar</a>
+      </td>
     </tr>
     @endforeach
   </tbody>
